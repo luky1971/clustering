4bfda4266b680a14c9340c43d8587d9a76d9a72c
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2017-10-18 16:30:44 -0400

    Created spectral clustering script, fixed count clusters script

--- countclusters.sh
+++ countclusters.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cut -f2 -d' ' $1 | uniq | wc -l
+cut -f2 -d' ' $1 | sort | uniq | wc -l
--- spectral.py
+++ spectral.py
@@ -1,9 +1,9 @@
 # Performs affinity propagation clustering on an input affinity matrix.
 # Arg 1: input file name
-# Arg 2: damping
-# Arg 3: max number of iterations
+# Arg 2: number of clusters
+# Arg 3: strategy used to assign embedding space labels, ‘kmeans’ or ‘discretize’
 import sys
-from sklearn.cluster import AffinityPropagation
+from sklearn.cluster import SpectralClustering
 from util import csv_to_matrix, replace_inf, print_list
 
 if len(sys.argv) < 2:
@@ -11,17 +11,17 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 infile = sys.argv[1]
-damping = 0.5
-max_iter = 200
+n_clusters = 8
+assign_labels = 'kmeans'
 
 if len(sys.argv) > 2:
-    damping = float(sys.argv[2])
+    n_clusters = int(sys.argv[2])
 
 if len(sys.argv) > 3:
-    max_iter = int(sys.argv[3])
+    assign_labels = sys.argv[3]
 
 x = replace_inf(csv_to_matrix(infile))
 
 if x is not None:
-    y = AffinityPropagation(damping=damping, max_iter=max_iter, affinity='precomputed').fit_predict(x)
+    y = SpectralClustering(n_clusters=n_clusters, affinity='precomputed', assign_labels=assign_labels).fit_predict(x)
     print_list(y)
