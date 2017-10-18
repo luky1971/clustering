49562ef00b5cb85058a0d82227b85367dcf7a95e
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2017-10-18 01:26:00 -0400

    Initial commit, created 1div script

---
+++ 1divcsv.py
@@ -0,0 +1,24 @@
+# This script reads a csv file, divides 1 by each element, and outputs the result.
+import csv
+import math
+import sys
+
+if len(sys.argv) < 3:
+    print('csv input and output file names, respectively, must be provided!')
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+
+try:
+    with open(infile) as csvin:
+        csvreader = csv.reader(csvin, delimiter=',')
+
+        try:
+            with open(outfile, 'w') as csvout:
+                csvwriter = csv.writer(csvout, delimiter=',')
+                for row in csvreader:
+                    csvwriter.writerow([(1/float(x)) if float(x) > sys.float_info.epsilon else math.inf for x in row])
+        except IOError:
+            print('Failed to open file for writing:', outfile)
+except IOError:
+    print('Failed to read', infile)
