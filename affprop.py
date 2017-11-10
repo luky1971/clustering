9718ccdedbf88dc4abdd12abe170efad2bf2cb64
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2017-11-10 11:26:26 -0500

    Added show centers to affprop

--- affprop.py
+++ affprop.py
@@ -2,6 +2,7 @@
 # Arg 1: input file name
 # Arg 2: damping
 # Arg 3: max number of iterations
+# Arg 4: (optional) flag '-centers' if you want to print the indexes of the cluster centers
 import sys
 from sklearn.cluster import AffinityPropagation
 from util import csv_to_matrix, replace_inf, print_list
@@ -20,8 +21,17 @@ if len(sys.argv) > 2:
 if len(sys.argv) > 3:
     max_iter = int(sys.argv[3])
 
+show_centers = False
+if len(sys.argv) > 4:
+    show_centers = sys.argv[4] == '-centers'
+
 x = replace_inf(csv_to_matrix(infile))
 
 if x is not None:
-    y = AffinityPropagation(damping=damping, max_iter=max_iter, affinity='precomputed').fit_predict(x)
-    print_list(y)
+    affprop = AffinityPropagation(damping=damping, max_iter=max_iter, affinity='precomputed')
+    y = affprop.fit_predict(x)
+    if show_centers:
+        for ind in affprop.cluster_centers_indices_:
+            print(ind)
+    else:
+        print_list(y)
